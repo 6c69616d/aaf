c697848b47a4097c894f606be79aa6e88e512568
6c69616d <44211257+6c69616d@users.noreply.github.com>
2019-02-06 12:08:00 +0000

    Moved all server related files in to a server folder
    Moved all server related files in to a server folder and added the assignment brief to the root to track progress

---
+++ Assignment/server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import { userRoutes } from './src/routes/userRoutes';
+import { fileRoutes } from './src/routes/fileRoutes';
+
+const app = express();
+const PORT = 3030;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/database', {
+    useNewUrlParser: true,
+});
+mongoose.connection.on('error', () => {
+    console.log('MongoDB connection error. Make sure the MongoDB is running.');
+});
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+userRoutes(app);
+fileRoutes(app);
+
+app.get('/', (req, res) => res.send(`Node and express server is running on ${PORT}`));
+
+app.listen(PORT, () => console.log(`your server is running on ${PORT}`));
