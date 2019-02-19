4541a2e6f57848dee540b20ac00dac3bde3744ca
6c69616d <44211257+6c69616d@users.noreply.github.com>
2019-02-19 19:09:22 +0000

    Completed validation on edit file form

--- Assignment/client/src/components/EditSpecificFile.vue
+++ Assignment/client/src/components/EditSpecificFile.vue
@@ -4,7 +4,7 @@
       <md-toolbar>
         <h1 class="md-title">Edit File</h1>
       </md-toolbar>
-      <form id="form" novalidate @submit.prevent>
+      <form id="form" novalidate @submit.prevent="validateForm(file._id, file.metadata[file.metadata.length - 1])">
         <md-field>
           <label>Name With File Type</label>
           <md-input type="text" disabled v-model="file.nameWithFileType"></md-input>
@@ -21,18 +21,24 @@
           <label>Mime Type</label>
           <md-input type="text" disabled v-model="file.mime_type"></md-input>
         </md-field>
-        <md-field>
+        <md-field :class="getValidationClass('title')">
           <label for="title">Title</label>
-          <md-input type="text" name="title" id="title" v-model="file.metadata[file.metadata.length - 1].title"></md-input>
-        
+          <md-input
+            type="text"
+            name="title"
+            id="title"
+            v-model="file.metadata[file.metadata.length - 1].title" @keyup="updateFormProperties(file)"
+          ></md-input>
+          <span class="md-error" v-if="!$v.form.title.required">The title is required</span>
         </md-field>
         <md-field>
           <label>Version Number</label>
           <md-input type="number" v-model="file.metadata[file.metadata.length - 1].version_number"></md-input>
         </md-field>
-        <md-field>
+        <md-field :class="getValidationClass('version_author')">
           <label>Version Author</label>
-          <md-input type="text" v-model="file.metadata[file.metadata.length - 1].version_author"></md-input>
+          <md-input type="text" name="version_author" v-model="file.metadata[file.metadata.length - 1].version_author" @keyup="updateFormProperties(file)"></md-input>
+          <span class="md-error" v-if="!$v.form.version_author.required">The author is required</span>
         </md-field>
 
         <label>Version Date</label>
@@ -48,34 +54,46 @@
           <label>File Size (MB)</label>
           <md-input type="number" v-model="file.metadata[file.metadata.length - 1].file_size"></md-input>
         </md-field>
-        <md-button type="submit" class="md-raised md-primary" @click="validateUser()">Edit</md-button>
+        <md-button type="submit" class="md-raised md-primary">Edit</md-button>
       </form>
     </md-card>
   </div>
 </template>
 <script>
+import { validationMixin } from "vuelidate";
+import { required } from "vuelidate/lib/validators";
 export default {
   name: "EditSpecificFile",
+  mixins: [validationMixin],
   data() {
     return {
-      file: {}
+      file: {},
+      form: {
+        title: '',
+        version_author: '',
+      }
     };
   },
-  mounted() {
-    const currentUrl = window.location.pathname.split("/");
-    const fileId = currentUrl[2];
-    this.$axios
-      .put(`http://localhost:3030/files/${fileId}`, { locked: true });
-    this.$axios
-      .get(`http://localhost:3030/files/${fileId}`) // Pass in ID as param
-      .then(response => {
-        this.file = response.data;
-      })
-      .catch(error => {
-        console.log("Data Reterival Failed");
-      });
+  
+  validations: {
+    form: {
+      title: {
+        required
+      },
+      version_author: {
+        required
+      }
+    }
   },
   methods: {
+    updateFormProperties(file) {
+      const fileMetadata = file.metadata[file.metadata.length - 1];
+      Object.keys(this.form).forEach((prop) => {
+        if (fileMetadata.hasOwnProperty(prop)) {
+          this.form[prop] = fileMetadata[prop];
+        }
+      });
+    },
     submitEditSpecificFile(fileId, newVersion) {
       this.$axios
         .put(`http://localhost:3030/files/${fileId}`, {
@@ -94,7 +112,35 @@ export default {
           console.log("Unable to process new version");
         });
     },
-}
+    validateForm(fileId, file) {
+      this.$v.form.$touch();
+      if(this.$v.form.$error) return
+      this.submitEditSpecificFile(fileId, file);
+    },
+    getValidationClass(fieldName) {
+      const field = this.$v.form[fieldName];
+
+      if (field) {
+        return {
+          "md-invalid": field.$error
+        };
+      }
+    }
+  },
+  mounted() {
+    const currentUrl = window.location.pathname.split("/");
+    const fileId = currentUrl[2];
+    this.$axios.put(`http://localhost:3030/files/${fileId}`, { locked: true });
+    this.$axios
+      .get(`http://localhost:3030/files/${fileId}`) // Pass in ID as param
+      .then(response => {
+        this.file = response.data;
+        this.updateFormProperties(response.data);
+      })
+      .catch(error => {
+        console.log("Data Reterival Failed");
+      });
+  },
 };
 </script>
 
